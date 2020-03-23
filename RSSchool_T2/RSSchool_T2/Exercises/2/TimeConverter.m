5b82bde8373fbfed187fa2ad3b0885601ea8decb
elya701 <52858764+elya701@users.noreply.github.com>
2020-03-23 21:50:19 +0300

    fix: fixed NSNumberFormatter in exercise 2

--- RSSchool_T2/RSSchool_T2/Exercises/2/TimeConverter.m
+++ RSSchool_T2/RSSchool_T2/Exercises/2/TimeConverter.m
@@ -5,65 +5,83 @@
 - (NSString*)convertFromHours:(NSString*)hours minutes:(NSString*)minutes {
     NSMutableString* result = [NSMutableString new];
     int minutesInt = minutes.intValue;
-    NSNumberFormatter *f = [NSNumberFormatter new];
-    f.numberStyle = NSNumberFormatterDecimalStyle;
-    NSNumber *hoursNumber = [f numberFromString:hours];
-    NSNumber *minutesNumber = [f numberFromString:minutes];
-    
-    NSNumberFormatter* numberFormatter = [NSNumberFormatter new];
-    [numberFormatter setNumberStyle:NSNumberFormatterSpellOutStyle];
-    NSString* hoursConverted = [numberFormatter stringFromNumber:hoursNumber];
-    NSString* minutesConverted = [numberFormatter stringFromNumber:minutesNumber];
-    int ourMinutes = 60 - minutesInt;
-    NSNumber* minutesConvertedAf = [NSNumber numberWithInt:ourMinutes];
-    NSString* minutesConvertedAfter = [numberFormatter stringFromNumber:minutesConvertedAf];
-    
-    int ourHours = hours.intValue + 1;
-    NSNumber* hoursConvertedAf = [NSNumber numberWithInt:ourHours];
-    NSString* hoursConvertedAfter = [numberFormatter stringFromNumber:hoursConvertedAf];
+ 
+    NSDictionary *dictionary = @{
+        @"1" : @"one",
+        @"2" : @"two",
+        @"3" : @"three",
+        @"4" : @"four",
+        @"5" : @"five",
+        @"6" : @"six",
+        @"7" : @"seven",
+        @"8" : @"eight",
+        @"9" : @"nine",
+        @"10" : @"ten",
+        @"11" : @"eleven",
+        @"12" : @"twelve",
+        @"13" : @"thirteen",
+        @"14" : @"fourteen",
+        @"15" : @"fifteen",
+        @"16" : @"sixteen",
+        @"17" : @"seventeen",
+        @"18" : @"eighteen",
+        @"19" : @"nineteen",
+        @"20" : @"twenty",
+        @"21" : @"twenty one",
+        @"22" : @"twenty two",
+        @"23" : @"twenty three",
+        @"24" : @"twenty four",
+        @"25" : @"twenty five",
+        @"26" : @"twenty six",
+        @"27" : @"twenty seven",
+        @"28" : @"twenty eight",
+        @"29" : @"twenty nine"
+    };
     
     if (minutesInt == 0) {
-        [result appendString:hoursConverted];
+        [result appendString:dictionary[hours]];
         [result appendString:@" o' clock"];
-        NSString* res = [result stringByReplacingOccurrencesOfString:@"-" withString:@" "];
-        return res;
+        return result;
     }
     
     if (minutesInt == 15) {
         [result appendString:@"quarter past "];
-        [result appendString:hoursConverted];
-        NSString* res = [result stringByReplacingOccurrencesOfString:@"-" withString:@" "];
-        return res;
+        [result appendString:dictionary[hours]];
+        return result;
     }
     
     if (minutesInt == 30) {
         [result appendString:@"half past "];
-        [result appendString:hoursConverted];
-        NSString* res = [result stringByReplacingOccurrencesOfString:@"-" withString:@" "];
-        return res;
+        [result appendString:dictionary[hours]];
+        return result;
     }
     
     if (minutesInt == 45) {
+        int hoursInt = [hours intValue] + 1;
+        NSString* hoursNew = [NSString stringWithFormat:@"%d", hoursInt];
         [result appendString:@"quarter to "];
-        [result appendString:hoursConvertedAfter];
-        NSString* res = [result stringByReplacingOccurrencesOfString:@"-" withString:@" "];
-        return res;
+        [result appendString:dictionary[hoursNew]];
+        return result;
     }
     
     if (minutesInt < 30) {
-        [result appendString:minutesConverted];
+        int minutesInt = [minutes intValue];
+        NSString* minutesNew = [NSString stringWithFormat:@"%d", minutesInt];
+        [result appendString:dictionary[minutesNew]];
         [result appendString:@" minutes past "];
-        [result appendString:hoursConverted];
-        NSString* res = [result stringByReplacingOccurrencesOfString:@"-" withString:@" "];
-        return res;
+        [result appendString:dictionary[hours]];
+        return result;
     }
     
     if (minutesInt > 30 && minutesInt < 60) {
-        [result appendString:minutesConvertedAfter];
+        int minutesInt = 60 - [minutes intValue];
+        NSString* minutesNew = [NSString stringWithFormat:@"%d", minutesInt];
+        int hoursInt = [hours intValue] + 1;
+        NSString* hoursNew = [NSString stringWithFormat:@"%d", hoursInt];
+        [result appendString:dictionary[minutesNew]];
         [result appendString:@" minutes to "];
-        [result appendString:hoursConvertedAfter];
-        NSString* res = [result stringByReplacingOccurrencesOfString:@"-" withString:@" "];
-        return res;
+        [result appendString:dictionary[hoursNew]];
+        return result;
     }
     
     return @"";
